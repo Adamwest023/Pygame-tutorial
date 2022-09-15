14c307c726da576f55f3414c1066196885cf40b9
Adam West <Adamwest023@gmail.com>
2022-09-15 12:57:30 -0500

    added assets

--- pygame-tutorial.py
+++ pygame-tutorial.py
@@ -7,12 +7,19 @@ pygame.display.set_caption("Runner Game")
 
 # clock object for framerate
 clock = pygame.time.Clock()
+
+#create a surface
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
 while True:
     # event(for) loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    screen.blit(test_surface, (200,100))
+            
     # draw all out elements
     # update everything
     pygame.display.update()
