8d6a1eaf717d5e2c90f29dda057a38ea4538da6c
Adam West <Adamwest023@gmail.com>
2022-09-15 13:18:00 -0500

    added movement to snail

--- pygame-tutorial.py
+++ pygame-tutorial.py
@@ -11,9 +11,12 @@ clock = pygame.time.Clock()
 # create a surface
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+# create text and surface for it
+text_surface = test_font.render('My game', False, 'Black')
 
-# create text and surface for it 
-text_surface = test_font.render('My game',False, 'Black')
+# create snail surface and movement
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
 while True:
     # event(for) loop
@@ -21,11 +24,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface,(300,50))       
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if(snail_x_pos <= -70):
+        snail_x_pos = 850
+    screen.blit(snail_surface, (snail_x_pos, 270))
     # draw all out elements
     # update everything
     pygame.display.update()
     clock.tick(60)
-    
