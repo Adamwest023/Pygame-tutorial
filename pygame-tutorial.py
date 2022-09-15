a62b250f2eb32dc437fedfda0541a06751f5423d
Adam West <Adamwest023@gmail.com>
2022-09-15 13:09:10 -0500

    added text layer

--- pygame-tutorial.py
+++ pygame-tutorial.py
@@ -4,13 +4,16 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner Game")
-
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 # clock object for framerate
 clock = pygame.time.Clock()
 
-#create a surface
+# create a surface
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+
+# create text and surface for it 
+text_surface = test_font.render('My game',False, 'Black')
 
 while True:
     # event(for) loop
@@ -19,8 +22,8 @@ while True:
             pygame.quit()
             exit()
     screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,270))
-            
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300,50))       
     # draw all out elements
     # update everything
     pygame.display.update()
