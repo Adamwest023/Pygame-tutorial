fb1fe6f6bb028900e8bc5ec9af6842126a1843b2
Adam West <Adamwest023@gmail.com>
2022-09-21 10:31:32 -0500

    added end game restart

--- pygame-tutorial.py
+++ pygame-tutorial.py
@@ -36,12 +36,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = -20
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
     if game_active:
         # draw all out elements
@@ -71,4 +76,3 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
- 
