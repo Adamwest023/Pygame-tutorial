f630dd0dc538581c90d41303ec36b44be47f8ee5
Adam West <Adamwest023@gmail.com>
2022-09-26 11:07:25 -0500

    code cleanup and enemy randomization

--- pygame-tutorial.py
+++ pygame-tutorial.py
@@ -13,7 +13,6 @@ def display_score():
 
 
 def intro_screen():
-    
     game_name = test_font.render('Pixel Game', False, (111, 196, 169))
     game_name_rect = game_name.get_rect(center=(400, 50))
     game_message = test_font.render('Press SPACE to run', False, (111, 196, 169))
@@ -40,13 +39,12 @@ score = 0
 sky_surf = pygame.image.load('graphics/Sky.png').convert()
 ground_surf = pygame.image.load('graphics/ground.png').convert()
 
-# create text and surface for it
-# score_surf = test_font.render('My game', False, (64, 64, 64))
-# score_rect = score_surf.get_rect(center=(400, 50))
 # create snail surface and movement
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-
 snail_rect = snail_surf.get_rect(midbottom=(600, 300))
+
+# Obstacles 
+obstacle_rect_list = []
 
 # player surface and rectangle
 player_surf = pygame.image.load(
@@ -60,6 +58,9 @@ player_stand = pygame.image.load(
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
 
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,900)
 
 while True:
     # event(for) loop
@@ -67,6 +68,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+            
         if game_active:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
@@ -74,6 +76,8 @@ while True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
                     player_gravity = -20
+            if event.type == obstacle_timer:
+                print('Test')
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
@@ -84,16 +88,7 @@ while True:
         # draw all out elements
         screen.blit(sky_surf, (0, 0))
         screen.blit(ground_surf, (0, 300))
-        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 12)
-        # screen.blit(score_surf, score_rect)
-        # pygame.draw.line(screen, (64, 64, 64), (340, 65), (460, 65), 2)
         score = display_score()
-        snail_rect.x -= 4
-        if snail_rect.right <= -30:
-            snail_rect.left = 850
-        screen.blit(snail_surf, snail_rect)
-
         # Player
         player_gravity += 1
         player_rect.y += player_gravity
